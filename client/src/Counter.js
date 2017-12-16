a9c49afb868e0f59f2140f64e58c4c680d5e9a7f
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2017-12-16 13:27:59 -0500

    made counter component

--- client/src/Counter.js
+++ client/src/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Counter extends Component {
 	constructor() {
@@ -7,7 +8,10 @@ class Counter extends Component {
 	}
 
 	componentWillMount() {
-
+		axios.get('/')
+		.then(({ data }) => {
+			this.setState(data);
+		})
 	}
 
 	render() {
