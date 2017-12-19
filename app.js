e6ad0cb4b254da633759e481990e97c14dfad00a
Sunit Kulkarni <25267606+Sunit-Kulkarni@users.noreply.github.com>
2017-12-19 16:28:28 -0500

    add .js extension

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var counter = require('/routes/counter');
+var counter = require('/routes/counter.js');
 
 var app = express();
 
